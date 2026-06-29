fe1bb7d0355f8ccc82d2a0b0a4cbc4c8e81001b7
Lucas Paiva <paival907@gmail.com>
2026-06-29 18:19:44 -0300

    att

---
+++ api/src/server.js
@@ -0,0 +1,14 @@
+
+require("dotenv").config();
+
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.listen(process.env.PORT, () => {
+    console.log(`Servidor rodando na porta ${process.env.PORT}`);
+});
